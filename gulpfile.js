b0fbf72977d71626a06771baf9d8958b121e233a
Viktoriia <frankenweenie1984@gmail.com>
2025-10-29 19:37:43 +0200

    remove unused imports

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,11 @@ exports.libs_style = tasks.libs_style;
 exports.svg_css = tasks.svg_css;
 exports.fonts = tasks.fonts;
 exports.style = tasks.style;
-exports.build_js = tasks.build_js;
 exports.html = tasks.html;
 exports.php = tasks.php;
 exports.rastr = tasks.rastr;
 exports.webp = tasks.webp;
 exports.svg_sprite = tasks.svg_sprite;
-exports.ttf = tasks.ttf;
-exports.ttf2 = tasks.ttf2;
 exports.bs_html = tasks.bs_html;
 exports.bs_php = tasks.bs_php;
 exports.watch = tasks.watch;
@@ -22,12 +19,8 @@ exports.deploy = tasks.deploy;
 exports.default = gulp.parallel(
   exports.libs_style,
   exports.svg_css,
-  exports.ttf,
-  exports.ttf2,
   exports.fonts,
   exports.style,
-  exports.libs_js,
-  exports.dev_js,
   exports.rastr,
   exports.webp,
   exports.svg_sprite,
@@ -38,12 +31,8 @@ exports.default = gulp.parallel(
 exports.dev_php = gulp.parallel(
   exports.libs_style,
   exports.svg_css,
-  exports.ttf,
-  exports.ttf2,
   exports.fonts,
   exports.style,
-  exports.libs_js,
-  exports.dev_js,
   exports.rastr,
   exports.webp,
   exports.svg_sprite,
